07c22e57ee2d373424a7b62424fde4ca3d8a8fe9
afauzann <afauzan857@gmail.com>
2022-11-07 10:53:39 +0700

    src/pages: add CartPage

---
+++ src/pages/CartPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Cart from "../component/Cart/Cart";
+import Footer from "../component/Footer/Footer";
+import Example from "../component/Navbar/Navbar";
+
+const CartPage = () => {
+  return (
+    <>
+      <Example />
+      <br></br>
+      <Cart />
+      <br></br>
+      <Footer />
+    </>
+  );
+};
+
+export default CartPage;
