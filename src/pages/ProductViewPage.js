b28853af9dab6b079e90f090e2ac52fc715bb2b7
afauzann <afauzan857@gmail.com>
2022-11-07 10:48:23 +0700

    src/pages: add ProductViewPage

---
+++ src/pages/ProductViewPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../component/Footer/Footer";
+import Example from "../component/Navbar/Navbar";
+import ProductView from "../component/ProductView/ProductView";
+
+const ProductViewPage = () => {
+  return (
+    <>
+      <Example />
+      <ProductView />
+      <br></br>
+      <Footer />
+    </>
+  );
+};
+
+export default ProductViewPage;
